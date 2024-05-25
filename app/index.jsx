780b831a58e8f4465cbf7af122f018059bda3839
Durga Potnuru <vijaypotnuru123@gmail.com>
2024-05-25 13:29:32 +0530

    chore: Added SplashScreen and font loading to RootLayout, Bottom Navigation Bar

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,17 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-
 import { View, Text, Image, ScrollView } from "react-native";
-
-
-
+import { Tabs, Redirect } from "expo-router";
 
 const App = () => {
-
-
   return (
-   <View>
-    <Text>HULK</Text>
-   </View>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/home" style={{ color: "blue" }}>
+        Go to Home
+      </Link>
+    </View>
   );
 };
 
